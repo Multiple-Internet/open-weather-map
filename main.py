7af4a37db37434e31f895975e6cdeafcd57afb15
Kelly Wright <kelly.wright@kellyinnovation.com>
2016-09-09 14:40:24 -0400

    Split date text.

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def main():
 	country = input("What is the country?\n >")
 	location = city + "," + country.lower()
 
-	print(location)
+
 
 	payload = {
 		"appid": api_key,
@@ -25,11 +25,27 @@ def main():
 
 	print(r.url)
 	weather = r.json()
+	
+	print("Today's Weather Conditions \n")
+
 	print("Current Temperature: {}".format(weather["main"]["temp"]))
 	print("Pressure: {}".format(weather["main"]["pressure"]))
 	print("Humidity: {}".format(weather["main"]["humidity"]))
 	print("Today's Low: {}".format(weather["main"]["temp_min"]))
 	print("Today's High: {}".format(weather["main"]["temp_max"]))
+
+	f = requests.get("http://api.openweathermap.org/data/2.5/forecast", params=payload)
+
+	print(f.url)
+	forecast = f.json()
+
+	forecast_list = forecast["list"]
+
+	for index in range(len(forecast["list"])):
+		date_variable = (forecast["list"][index]["dt_txt"]).split(" ")
+		print(date_variable[0])
+
+	print("Here is the 5-Day Forecast.")
 
 
 
